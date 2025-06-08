8f5397ea213d583f5bb8ce25e00068bcd3463415
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2025-06-08 22:51:09 +0530

    Changed Image Path

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import reactLogo from '../assets/React-JS-IMG.png'
 
 const HeaderSection = () => {
   return (
     <header className='w-screen h-auto bg-transparent flex justify-center gap-10 md:gap-20 items-center pt-5'>
         <div>
-            <img className='w-28 h-20 md:w-32 md:h-32' src="assets\React-JS-IMG.png" alt="" />
+            <img className='w-28 h-20 md:w-32 md:h-32' src={reactLogo} alt="" />
         </div>
         <div>
             <h1 className=' text-[#f7f8ff] text-2xl md:text-5xl'> The React Practice Quiz</h1>
