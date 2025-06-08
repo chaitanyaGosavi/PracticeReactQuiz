32d9c9dd314efba73848cff5bebc458c42e0afc0
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2025-06-08 20:18:33 +0530

    first commit

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -4,7 +4,7 @@ const HeaderSection = () => {
   return (
     <header className='w-screen h-auto bg-transparent flex justify-center gap-10 md:gap-20 items-center pt-5'>
         <div>
-            <img className='w-28 h-20 md:w-32 md:h-32' src="src\assets\React-JS-IMG.png" alt="" />
+            <img className='w-28 h-20 md:w-32 md:h-32' src=".\assets\React-JS-IMG.png" alt="" />
         </div>
         <div>
             <h1 className=' text-[#f7f8ff] text-2xl md:text-5xl'> The React Practice Quiz</h1>
